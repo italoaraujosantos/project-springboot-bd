eac0de8aaecf575659a0978dece7172073c7eff1
Italo Araujo Chaves <italoaraujosantos@gmail.com>
2026-08-04 21:04:30 -0300

    commit initial

---
+++ src/main/java/br/com/isac/projectspringbootbd/ProjectSpringbootBdApplication.java
@@ -0,0 +1,13 @@
+package br.com.isac.projectspringbootbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isac/projectspringbootbd/ProjectSpringbootBdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isac.projectspringbootbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
